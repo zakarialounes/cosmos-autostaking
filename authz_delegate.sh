621cc4b5c10e2c57ae3a584a97ebb4cbee246f8a
Zakaria Lounes <zakaria.lounes@gmail.com>
2022-06-27 03:13:11 +0200

    fix: gas is payed by authz operator

--- authz_delegate.sh
+++ authz_delegate.sh
@@ -38,10 +38,11 @@ delegateAction() {
   amountFinal=$(getFinalBalance ${balance} ${rewards})
   echo "------ DELEGATE ------"
   echo "Balance: ${balance}"
+  echo "Rewards: ${rewards}"
   echo "Amount to delegate: ${amountFinal}"
   if [[ ${amountFinal} > 0 ]]; then
     rm -rf tx.json
-    ${BINARY} tx staking delegate ${VALIDATOR} ${amountFinal}${DENOM} --from ${DELEGATOR} ${GAS_PRICES} ${DETAILS} -y --generate-only >tx.json
+    ${BINARY} tx staking delegate ${VALIDATOR} ${amountFinal}${DENOM} --from ${DELEGATOR} ${DETAILS} -y --generate-only >tx.json
     ${BINARY} tx authz exec tx.json --from ${RESTAKE} ${GAS_PRICES} ${DETAILS} -y
     rm -rf tx.json
   fi
